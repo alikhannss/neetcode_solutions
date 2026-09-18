17be6cff63a154f9ef6672bb31f6fd634b4011d9
Alikhan <111850170+alikhannss@users.noreply.github.com>
2026-09-18 16:54:32 +0500

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, ans = 0;
+        for (int num:nums) {
+            if (count == 0) {
+                ans = num;
+            }
+            if (ans == num) {
+                count++;
+            }else {
+                count--;
+            }
+        }
+        return ans;
+    }
+};
+// 5 4
+// 1 3
